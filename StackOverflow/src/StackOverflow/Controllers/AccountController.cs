634559b31ecd072e8499e8543ea206081f45f018
michael a. smith <michaelanthonysmith00@gmail.com>
2016-04-25 09:05:14 -0700

    set up backend files and identity requirements.

---
+++ StackOverflow/src/StackOverflow/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Identity;
+using StackOverflow.Models;
+
+
+namespace StackOverflow.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signIngManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signIngManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
